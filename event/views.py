64961324e2d5923d5d0b9502b5a0d25b7b38b574
dmelkk <dmelkk@gmail.com>
2015-02-20 12:09:35 +0200

    display events

--- event/views.py
+++ event/views.py
@@ -2,30 +2,53 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 from event.models import Event
 from event.forms import AddEventForm
 import datetime
 import time
 # Create your views here.
 
+def main(request):
+    user_events = Event.objects.filter(owner = request.user)
+    now = datetime.datetime.now().strftime("%a, %d. %b")
+    return render_to_response('main.html', {
+            'date': now,
+            'user_events': user_events
+            },
+            context_instance =
+            RequestContext(request)
+        ) 
+               
 @login_required
 def add_event(request): 
+    errors = []
     if request.method == 'POST':
-        form = AddEventForm(request.POST)
-        if form.is_valid():
-            cd = form.cleaned_data
-            e = Events(
-                evt_name=cd['name'], 
-                evt_place=cd['place'], 
-                evt_start_time=cd['time_start'], 
-                evt_end_time=cd['time_end'], 
-                evt_date=cd['date'], 
-                evt_note=cd['note']
+        if not request.POST.get('name', ''):
+            errors.append('Enter an eventname.')
+        if not request.POST.get('place', ''):
+            errors.append('Enter an eventplace.')
+        if not request.POST.get('start_time', ''):
+            errors.append('Enter a start time.')
+        if not request.POST.get('end_time', ''):
+            errors.append('Enter an end time.')
+        if not request.POST.get('date', ''):
+            errors.append('Enter a date.')
+        if not errors:
+            e = Event(
+                evt_name=request.POST['name'], 
+                evt_place=request.POST['place'], 
+                evt_start_time=request.POST['start_time'], 
+                evt_end_time=request.POST['end_time'], 
+                evt_date=request.POST['date'], 
+                evt_note=request.POST['note'],
+                owner=request.user
             )
             e.save()
             return HttpResponseRedirect('/')
-    else: 
-        form = AddEventForm(
-            initial={}
-        )
-    return render_to_response('add_event.html', {'form': form}, context_instance = RequestContext(request))
+    return render_to_response('add_event.html', {'errors': errors}, context_instance = RequestContext(request))
+
+@login_required
+def active_event(request, cur_id):
+    event = Event.objects.filter(id = cur_id)
+    return render_to_response('active_event.html', {'event': event})
